db8909ac528365e180bde60c1ea6cfd2950f57fb
Michael Cantley <mjcantle@umich.edu>
2018-04-10 14:37:40 -0400

    4.10

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import model
 
 app = Flask(__name__)
@@ -11,11 +11,43 @@ def index():
         <ul>
             <li><a href="/bball"> Men's Basketball </a></li>
         </ul>
+        <ul>
+            <li><a href="/football"> Men's Football </a></li>
+        </ul>
     '''
 
-@app.route('/bball')
+@app.route('/bball', methods=['GET', 'POST'])
 def bball():
-    return render_template("seasons.html", seasons=model.get_bball_seasons())
+    if request.method == 'POST':
+        sortby = request.form['sortby']
+        sortorder = request.form['sortorder']
+        seasons = model.get_bball_seasons(sortby, sortorder)
+    else:
+        seasons = model.get_bball_seasons()
+    return render_template("seasons.html", seasons=seasons)
+
+@app.route('/football', methods=['GET', 'POST'])
+def football():
+    if request.method == 'POST':
+        sortby = request.form['sortby']
+        sortorder = request.form['sortorder']
+        seasons = model.get_football_seasons(sortby, sortorder)
+    else:
+        seasons = model.get_football_seasons()
+    return render_template("Football_season.html", seasons=Football_season)
+
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    if request.method == "POST":
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+    else:
+        firstname = ''
+        lastname = ''
+    return render_template("hello.html", firstname=firstname, lastname=lastname)
+
 
 if __name__ == '__main__':
+    model.init_bball()
     app.run(debug=True)
